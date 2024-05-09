50c03102ba89bc545c259d4ed322fa10cb8db0e9
rianna <riannakiilu@gmail.com>
2024-05-09 12:33:34 +0300

    navbar posted

---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+const Navbar=()=>{
+    return(
+      <div>
+        <h1>Navbar</h1>
+        <nav>
+            <ul>
+                <li href="">Home</li>
+                <li href="">Creat</li>
+            </ul>
+        </nav>
+      </div>
+    );
+}
+export default Navbar;
